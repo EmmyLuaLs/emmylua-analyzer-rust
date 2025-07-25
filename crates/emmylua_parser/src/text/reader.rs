39e06017b167510afa67f976492a5a4a67da12bb
Tamika Nomara <taminomara@gmail.com>
2025-07-25 16:03:48 +0400

    Add `ReaderWithMarks` and `ReaderBacktrackPoint`

--- crates/emmylua_parser/src/text/reader.rs
+++ crates/emmylua_parser/src/text/reader.rs
@@ -1,4 +1,8 @@
 use super::text_range::SourceRange;
+use crate::parser::{MarkEvent, MarkerEventContainer};
+use crate::LuaTokenKind;
+use std::mem::forget;
+use std::ops::{Deref, DerefMut};
 use std::str::Chars;
 
 pub const EOF: char = '\0';
@@ -196,6 +200,194 @@ impl<'a> Default for Reader<'a> {
     }
 }
 
+/// Reader that can emit token marks and events into the given container.
+///
+/// Used for backtracking.
+#[derive(Debug)]
+pub struct ReaderWithMarks<'a> {
+    reader: Reader<'a>,
+    events: &'a mut Vec<MarkEvent>,
+}
+
+#[allow(dead_code)]
+impl<'a> ReaderWithMarks<'a> {
+    pub fn new(reader: Reader<'a>, events: &'a mut Vec<MarkEvent>) -> Self {
+        Self { reader, events }
+    }
+
+    pub fn bump(&mut self) {
+        self.reader.bump()
+    }
+
+    pub fn reset_buff(&mut self) {
+        self.reader.reset_buff()
+    }
+
+    pub fn reset_buff_into_sub_reader(&mut self) -> Reader<'a> {
+        self.reader.reset_buff_into_sub_reader()
+    }
+
+    pub fn emit(&mut self, kind: LuaTokenKind) {
+        let range = self.current_range();
+        if !range.is_empty() {
+            self.get_events().push(MarkEvent::EatToken { kind, range });
+        }
+        self.reset_buff();
+    }
+
+    pub fn is_eof(&self) -> bool {
+        self.reader.is_eof()
+    }
+
+    pub fn is_start_of_line(&self) -> bool {
+        self.reader.is_start_of_line()
+    }
+
+    pub fn prev_char(&self) -> char {
+        self.reader.prev_char()
+    }
+
+    pub fn current_char(&self) -> char {
+        self.reader.current_char()
+    }
+
+    pub fn next_char(&mut self) -> char {
+        self.reader.next_char()
+    }
+
+    pub fn current_range(&self) -> SourceRange {
+        self.reader.current_range()
+    }
+
+    pub fn tail_range(&self) -> SourceRange {
+        self.reader.tail_range()
+    }
+
+    pub fn current_text(&self) -> &'a str {
+        self.reader.current_text()
+    }
+
+    pub fn tail_text(&self) -> &'a str {
+        self.reader.tail_text()
+    }
+
+    pub fn eat_when(&mut self, ch: char) -> usize {
+        self.reader.eat_when(ch)
+    }
+
+    pub fn consume_char_n_times(&mut self, ch: char, count: usize) -> usize {
+        self.reader.consume_char_n_times(ch, count)
+    }
+
+    pub fn consume_n_times<F>(&mut self, func: F, count: usize) -> usize
+    where
+        F: Fn(char) -> bool,
+    {
+        self.reader.consume_n_times(func, count)
+    }
+
+    pub fn eat_while<F>(&mut self, func: F) -> usize
+    where
+        F: Fn(char) -> bool,
+    {
+        self.reader.eat_while(func)
+    }
+
+    pub fn get_source_text(&self) -> &'a str {
+        self.reader.get_source_text()
+    }
+
+    pub fn get_current_end_pos(&self) -> usize {
+        self.reader.get_current_end_pos()
+    }
+
+    pub fn backtrack_point<'s>(&'s mut self) -> ReaderBacktrackPoint<'s, 'a> {
+        ReaderBacktrackPoint::new(self)
+    }
+}
+
+impl<'a> MarkerEventContainer for ReaderWithMarks<'a> {
+    fn get_mark_level(&self) -> usize {
+        0
+    }
+
+    fn incr_mark_level(&mut self) {
+        // Nothing
+    }
+
+    fn decr_mark_level(&mut self) {
+        // Nothing
+    }
+
+    fn get_events(&mut self) -> &mut Vec<MarkEvent> {
+        &mut self.events
+    }
+}
+
+/// A backtrack point.
+///
+/// Used to easily backtrack any events or tokens that were read/emitted
+/// via [`ReaderWithMarks`].
+#[must_use = "backtrack points use RAII and should be saved on the stack"]
+pub struct ReaderBacktrackPoint<'a, 'b> {
+    // Note: we rely on the fact that `Reader` is trivially destructible,
+    // and forgetting `ReaderBacktrackPoint` without running its destructor
+    // will not leak memory. This saves us from storing an additional flag
+    // that tracks whether this point was committed or not. See `Drop` impl
+    // for more info.
+    reader: &'a mut ReaderWithMarks<'b>,
+    prev_state: Reader<'b>,
+    events_pos: usize,
+}
+
+impl<'a, 'b> ReaderBacktrackPoint<'a, 'b> {
+    pub fn new(reader: &'a mut ReaderWithMarks<'b>) -> Self {
+        // Note: even though `Reader` doesn't implement `copy`, it's still
+        // very cheap to clone as it doesn't own any memory.
+        let prev_state = reader.reader.clone();
+        let mark_pos = reader.events.len();
+        Self {
+            reader,
+            prev_state,
+            events_pos: mark_pos,
+        }
+    }
+
+    pub fn commit(self) {
+        debug_assert!(
+            !std::mem::needs_drop::<Reader>(),
+            "reader should be trivially destructible, \
+            otherwise this code may leak memory"
+        );
+        forget(self); // Don't run drop glue to avoid rollback.
+    }
+
+    pub fn rollback(self) {
+        drop(self); // Rollback happens automatically on drop.
+    }
+}
+
+impl<'a, 'b> Drop for ReaderBacktrackPoint<'a, 'b> {
+    fn drop(&mut self) {
+        self.reader.events.truncate(self.events_pos);
+        self.reader.reader = self.prev_state.clone();
+    }
+}
+
+impl<'a, 'b> Deref for ReaderBacktrackPoint<'a, 'b> {
+    type Target = ReaderWithMarks<'b>;
+
+    fn deref(&self) -> &Self::Target {
+        self.reader
+    }
+}
+
+impl<'a, 'b> DerefMut for ReaderBacktrackPoint<'a, 'b> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.reader
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -292,5 +484,55 @@ mod tests {
         let count = reader.eat_while(|c| c.is_ascii_digit());
         assert_eq!(count, 5);
         assert_eq!(reader.current_char(), 'H');
+    }
+
+    #[test]
+    fn test_backtrack_commit() {
+        let text = "Hello, world!";
+        let mut events = Vec::new();
+        let mut reader = ReaderWithMarks::new(Reader::new(text), &mut events);
+
+        reader.eat_while(|c| c != ' ');
+        assert_eq!(reader.current_text(), "Hello,");
+        {
+            let mut reader = reader.backtrack_point();
+            reader.eat_while(|_| true);
+            assert_eq!(reader.current_text(), "Hello, world!");
+            reader.commit();
+        }
+        assert_eq!(reader.current_text(), "Hello, world!");
+    }
+
+    #[test]
+    fn test_backtrack_rollback() {
+        let text = "Hello, world!";
+        let mut events = Vec::new();
+        let mut reader = ReaderWithMarks::new(Reader::new(text), &mut events);
+
+        reader.eat_while(|c| c != ' ');
+        assert_eq!(reader.current_text(), "Hello,");
+        {
+            let mut reader = reader.backtrack_point();
+            reader.eat_while(|_| true);
+            assert_eq!(reader.current_text(), "Hello, world!");
+            reader.rollback();
+        }
+        assert_eq!(reader.current_text(), "Hello,");
+    }
+
+    #[test]
+    fn test_backtrack_auto_rollback() {
+        let text = "Hello, world!";
+        let mut events = Vec::new();
+        let mut reader = ReaderWithMarks::new(Reader::new(text), &mut events);
+
+        reader.eat_while(|c| c != ' ');
+        assert_eq!(reader.current_text(), "Hello,");
+        {
+            let mut reader = reader.backtrack_point();
+            reader.eat_while(|_| true);
+            assert_eq!(reader.current_text(), "Hello, world!");
+        }
+        assert_eq!(reader.current_text(), "Hello,");
     }
 }
