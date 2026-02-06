13060a0063c9d0ee81c00e2f162b8a16c3964f72
CppCXY <812125110@qq.com>
2026-02-06 20:59:24 +0800

    update

--- crates/emmylua_code_analysis/src/db_index/schema/schema_file.rs
+++ crates/emmylua_code_analysis/src/db_index/schema/schema_file.rs
@@ -1,7 +1,29 @@
-use crate::FileId;
+use url::Url;
+
+use crate::LuaTypeDeclId;
 
 #[derive(Debug, Clone)]
 pub enum JsonSchemaFile {
     NeedResolve,
-    Resolved(FileId),
+    BadUrl,
+    Resolved(LuaTypeDeclId),
+}
+
+pub fn get_schema_short_name(url: &Url) -> String {
+    const MAX_LEN: usize = 128;
+
+    let url_str = url.as_str().to_string();
+    let mut new_name = String::new();
+    for c in url_str.chars().rev() {
+        if c == '/' || c == '#' || c == '?' || c == '&' || c == '=' {
+            new_name.push('_');
+        } else {
+            new_name.push(c);
+        }
+        if new_name.len() >= MAX_LEN {
+            break;
+        }
+    }
+
+    new_name.chars().rev().collect()
 }
