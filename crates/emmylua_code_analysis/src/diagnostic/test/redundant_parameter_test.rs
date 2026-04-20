3eb74cbc2e07a0f6a9ebe6b05d0125cdf22a7e8e
efve.zff <efve.zff@alibaba-inc.com>
2026-04-20 15:29:01 +0800

    test: add test cases for redundant-parameter with multi-return last arg

--- crates/emmylua_code_analysis/src/diagnostic/test/redundant_parameter_test.rs
+++ crates/emmylua_code_analysis/src/diagnostic/test/redundant_parameter_test.rs
@@ -176,4 +176,42 @@ mod test {
         "#
         ));
     }
+
+    #[test]
+    fn test_last_arg_multi_return_call() {
+        let mut ws = VirtualWorkspace::new_with_init_std_lib();
+
+        // last arg is a multi-return function call (table.unpack), should not report redundant-parameter
+        assert!(ws.check_code_for(
+            DiagnosticCode::RedundantParameter,
+            r#"
+            ---@param args table
+            local function f(args) end
+
+            ---@type any[]
+            local cmds = {}
+            f(cmds, table.unpack(cmds))
+        "#
+        ));
+    }
+
+    #[test]
+    fn test_last_arg_multi_return_call_with_alias() {
+        let mut ws = VirtualWorkspace::new_with_init_std_lib();
+
+        // table.unpack aliased to a local variable, should not report redundant-parameter
+        assert!(ws.check_code_for(
+            DiagnosticCode::RedundantParameter,
+            r#"
+            local tunpack = table.unpack
+
+            ---@param args table
+            local function f(args) end
+
+            ---@type any[]
+            local cmds = {}
+            f(cmds, tunpack(cmds, 2))
+        "#
+        ));
+    }
 }
